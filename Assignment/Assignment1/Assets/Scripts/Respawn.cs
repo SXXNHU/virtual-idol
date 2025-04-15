3d5be197b3b6dd0e34bd8f1d90ff94e43a88a4dd
SXXHU <dofqls0127@gmail.com>
2025-04-15 14:05:49 +0900

    FIX: commit all changes

--- Assignment/Assignment1/Assets/Scripts/Respawn.cs
+++ Assignment/Assignment1/Assets/Scripts/Respawn.cs
@@ -2,21 +2,29 @@ using UnityEngine;
 
 public class Respawn : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public Vector3 respawnPosition = new Vector3 (0, 3, 0);
-    public float fallThreshold = -5f;
-    void Start()
-    {
-        
-    }
+    public Vector3 respawnPosition = new Vector3(-92, 0, 56);
 
-    // Update is called once per frame
+    public float fallThreshold = -5f;
+
     void Update()
     {
-        if(transform.position.y < fallThreshold)
+        if (transform.position.y < fallThreshold)
         {
-            transform.position = respawnPosition;
-            GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+            Rigidbody rb = GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                // 위치와 회전 먼저 초기화
+                Debug.Log("[Respawn] teleporting to: " + respawnPosition);
+                transform.position = respawnPosition;
+                transform.rotation = Quaternion.identity;
+
+                // 속도 초기화
+                rb.linearVelocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+
+                // 물리 멈춤 처리
+                rb.Sleep();
+            }
         }
     }
 }
